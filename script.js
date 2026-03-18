b714176837189acf17dcd41477dceb8a69503571
KrisvHeij <49883345+KrisvHeij@users.noreply.github.com>
2026-03-18 22:45:58 +0100

    Updated

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const navBtn = document.getElementById("nav-button");
+const prevBtn = document.getElementById("previous-button");
+const nextBtn = document.getElementById("next-button");
 
-
-
+// Open/Close navigation
 if (navBtn) {
   const primaryNav = document.getElementById("primary-nav");
   const navIcon = navBtn.querySelector("img");
@@ -11,7 +12,26 @@ if (navBtn) {
     primaryNav.classList.toggle("show-primary-nav");
     darkBg.classList.toggle("visually-hidden");
     
-    primaryNav.classList.contains("show-primary-nav") ? navIcon.src = "./images/icon-close.svg" : navIcon.src = "./images/icon-menu.svg";
-    
-})
+    primaryNav.classList.contains("show-primary-nav") ? navIcon.src =        "./images/icon-close.svg" : navIcon.src = "./images/icon-menu.svg";   
+  })
 }
+
+// Image slider/gallery
+const btns = [prevBtn, nextBtn];
+const imageContainer = document.querySelector(".image-gallery-inner");
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const slide = e.target.closest(".image-button");
+
+    let currentIndex = 0;
+  
+    if (slide.id === "previous-button") {
+      currentIndex--;
+      imageContainer.style.transform = "translateX(-100%)";
+    }
+    if (slide.id === "next-button") {
+      currentIndex++;
+      imageContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
+    }
+  })
+})
